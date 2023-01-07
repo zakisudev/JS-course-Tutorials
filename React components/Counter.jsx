3d92c51d9844d4ece50810d01d676a0b09f8d3ef
zakisudev@gmail.com <zakisu250@gmail.com>
2023-01-07 09:10:55 +0300

    React Counter

---
+++ React components/Counter.jsx
@@ -0,0 +1,12 @@
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increment</button>
+    </div>
+  );
+};
+
+export default Counter;
