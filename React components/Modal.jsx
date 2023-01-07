507cbf5c441fb397e773384c5aa9404e64ce1f5d
zakisudev@gmail.com <zakisu250@gmail.com>
2023-01-07 09:10:55 +0300

    React Modal

---
+++ React components/Modal.jsx
@@ -0,0 +1,10 @@
+const Modal = ({ isOpen, onClose, children }) => {
+  return (
+    isOpen && (
+      <div>
+        <div>{children}</div>
+        <button onClick={onClose}>Close</button>
+      </div>
+    )
+  );
+};
