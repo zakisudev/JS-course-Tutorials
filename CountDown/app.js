bcacc575257aea4686b0c78c19de73ea40c673a4
Zekaria Mohammed <36282172+zakisu250@users.noreply.github.com>
2023-03-17 15:22:31 +0300

    Updated single number values

--- CountDown/app.js
+++ CountDown/app.js
@@ -11,11 +11,26 @@ function getTimeDifference(start, end) {
   let minutes = Math.floor(seconds / 60);
   let hours = Math.floor(minutes / 60 );
   let days = Math.floor(hours / 24 );
-
+  
   hours = hours - (days * 24);
   minutes = minutes - (days * 24 * 60) - (hours * 60);
   seconds = seconds - (days * 24 * 60 *60) - (hours * 60 * 60) - (minutes * 60);
-
+  if (days < 10)
+  {
+    days = "0" + days;
+  }
+  if (hours < 10)
+  {
+    hours = "0" + hours;
+  }
+  if (minutes < 10)
+  {
+    minutes = "0" + minutes;
+  }
+  if (seconds < 10)
+  {
+    seconds = "0" + seconds;
+  }
   return {
     rDays: days,
     rHours: hours,
